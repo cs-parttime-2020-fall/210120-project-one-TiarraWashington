2e697cebe85a101871227c7bca7f58640890bb42
TiarraWashington <kenniyah11@gmail.com>
2021-01-20 20:55:34 -0600

    working

--- script.js
+++ script.js
@@ -5,4 +5,24 @@ let assignedTo = document.querySelector("#assignedTo");
 let taskAssigned = document.querySelector("#taskAssigned");
 // selected checkbox using querySelector
 let istaskUrgent = document.querySelector("#istaskUrgent");
-let 
+//
+let buttn = document.querySelectorAll(".btn btn-primary");
+// 
+let buttton = document.querySelectorAll(".btn btn-danger");
+
+submitForm = (event) => {
+    event.preventDefault(); // stops page from reloading
+    console.log(assignedTo.value);
+    console.log(taskAssigned.value);
+    console.log(istaskUrgent.checked);
+    if (istaskUrgent.checked){ // will go back and correct
+        assignedTo.innerHTML += `<div>${assignedTo.value} : ${taskAssigned.value} `
+} else {
+    taskAssigned.innerHTML += `<div>${assignedTo.value} : ${taskAssigned.value} `
+}
+// clear values 
+assignedTo.value = ""
+taskAssigned.value = ""
+// 
+istaskUrgent.addEventListener("click", submitForm);
+
