411c69f1c7e91dbc73725c7941636e65c7c25987
TiarraWashington <kenniyah11@gmail.com>
2021-01-20 20:19:37 -0600

    selected input and istaskUrgent using querySelector

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 console.log(`21 01 20 HTML CSS JS Project One`);
 
+// selected input using querySelector
+let assignedTo = document.querySelector("#assignedTo");
+let taskAssigned = document.querySelector("#taskAssigned");
+// selected checkbox using querySelector
+let istaskUrgent = document.querySelector("#istaskUrgent");
+let 
