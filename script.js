079c216aa879ae4993a1934ba9e0714060938b75
TiarraWashington <kenniyah11@gmail.com>
2021-01-25 16:58:43 -0600

    forEach

--- script.js
+++ script.js
@@ -9,50 +9,48 @@ let submitBtn = document.querySelector("#submitBtn");
 
 // selected task in the urgent and non-urgent task list using querySelector
 let taskUrgency = document.querySelector("#taskUrgency");
-let urg = document.querySelector("#urg");
-let noturg = document.querySelector("#noturg");
+console.log(taskUrgency);
 
 // stuck on last two steps
-function hide(){
-for (i =0; i < taskUrgency.length; i++) { // iterate through task
-  while (i  ) {
-      tas
-  }
-    confirm("Do you want to delete clicked task");
-}
+function hide() {
+   
+    // iterate over items in the array
+    for (i=0; i < taskUrgency.length; i++){
+        // forEach
+        taskUrgency.forEach(confirm => confirm("confirm if you want to remove clicked task"));
 
-// used console log to make sure the correct elements are running in the browser
-// console.log(taskAssigned); console.log(taskdueDate); console.log(urg); console.log(noturg);
+    // used console log to make sure the correct elements are running in the browser
+    console.log(taskAssigned); console.log(taskdueDate); console.log(taskUrgency);
 
-// selected h4 using id of urgent task using querySelector
-let urgentTask = document.querySelector("#urgentTask");
-// console.log(urgentTask);
-// selected h4 using id of not urgent using querySelector
-let notUrgent = document.querySelector("#notUrgent");
-// console.log(notUrgent);
+    // selected h4 using id of urgent task using querySelector
+    let urgentTask = document.querySelector("#urgentTask");
+    // console.log(urgentTask);
+    // selected h4 using id of not urgent using querySelector
+    let notUrgent = document.querySelector("#notUrgent");
+    // console.log(notUrgent);
 
-submitForm = (event) => {
-    event.preventDefault(); // stops page from reloading
+    submitForm = (event) => {
+        event.preventDefault(); // stops page from reloading
 
-    // console logged information to make sure it was reading correctly in the console
-    console.log(taskAssigned.value);
-    console.log(taskdueDate.value);
-    console.log(taskUrgency.value);
+        // console logged information to make sure it was reading correctly in the console
+        console.log(taskAssigned.value);
+        console.log(taskdueDate.value);
+        console.log(taskUrgency.value);
 
-    // append values
-    if (taskUrgency.value == "Urgent") {
-        urgentTask.innerHTML += `<div> Task : ${taskAssigned.value} : Due Date : ${taskdueDate.value}`
-    } else {
-        notUrgent.innerHTML += `<div> Task : ${taskAssigned.value} : Due Date : ${taskdueDate.value}`
+        // append values
+        if (taskUrgency.value == "Urgent") {
+            urgentTask.innerHTML += `<div> Task : ${taskAssigned.value} : Due Date : ${taskdueDate.value}`
+        } else {
+            notUrgent.innerHTML += `<div> Task : ${taskAssigned.value} : Due Date : ${taskdueDate.value}`
+        }
+
+        // clear values from fields by referencing value equal to empty strings
+        taskAssigned.value = "";
+        taskdueDate.value = "";
+        // clear property from fields by referencing value name
+        taskUrgency.value = "Clear form";
     }
-  
-    // clear values from fields by referencing value equal to empty strings
-    taskAssigned.value = "";
-    taskdueDate.value = "";
-    // clear property from fields by referencing value name
-    taskUrgency.value = "Clear form";
-}
-// added an event listener that will output form submission
-submitBtn.addEventListener("click", submitForm);
-taskUrgency.addEventListener("click", hide);
-
+    // added an event listener that will output form submission
+    submitBtn.addEventListener("click", submitForm);
+    urgentTask.addEventListener("click", hide);
+    notUrgent.addEventListener("click", hide);
